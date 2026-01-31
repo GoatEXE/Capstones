4d4119a680d4d6584cd0aedb60576e1d37412cda
Dustin <45581066+GoatEXE@users.noreply.github.com>
2026-01-31 16:08:51 -0600

    Post page + path
    Also some slight CSS changes

--- Blog/index.js
+++ Blog/index.js
@@ -15,6 +15,14 @@ app.get('/', (req, res) => {
     res.render('index.ejs', { posts: posts});
 })
 
+app.get('/create', (req, res) => {
+    res.render('create.ejs');
+})
+
+app.post('/submit', (req, res) => {
+    console.log(req);
+})
+
 app.listen(port, () => {
     console.log(`Server is running on ${port}`);
 })
