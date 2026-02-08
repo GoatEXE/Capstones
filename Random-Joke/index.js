d970e020ebf671192a3af08feaa07006c76e48e6
Dustin <45581066+GoatEXE@users.noreply.github.com>
2026-02-07 18:40:19 -0600

    Initial Joke

---
+++ Random-Joke/index.js
@@ -0,0 +1,30 @@
+import express from "express";
+import axios from "axios";
+
+const app = express(); 
+const PORT = 3000;
+const pullLimit = 100;
+const API_URL = `https://www.reddit.com/r/jokes.json?limit=${pullLimit}`;
+
+app.use(express.static("public"));
+
+app.get("/", async (req, res) => {
+    try {
+        const response = await axios.get(API_URL);
+        const randomJoke = response.data.data.children[Math.floor(Math.random() * pullLimit)].data;
+        const joke = {
+            title: randomJoke.title,
+            body: randomJoke.selftext
+        }
+
+        res.render("index.ejs", { joke: joke });
+    }
+    catch (error) {
+        res.status(500).send("An error occurred while fetching the joke.");
+        console.log(error.message);
+    }
+})
+
+app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
+})
