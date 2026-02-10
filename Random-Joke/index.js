76d172fd12fd74e6fe4b0f69ae46f9bf17bd1a2b
Dustin <45581066+GoatEXE@users.noreply.github.com>
2026-02-09 22:50:20 -0600

    Add content/styling/animation
    Upvotes, comments, user content added with svg. Hover animation added.

--- Random-Joke/index.js
+++ Random-Joke/index.js
@@ -4,7 +4,7 @@ import axios from "axios";
 const app = express(); 
 const PORT = 3000;
 const pullLimit = 100;
-const API_URL = `https://www.reddit.com/r/jokes.json?limit=${pullLimit}`;
+const API_URL = `https://www.reddit.com/r/jokes.json?limit=${pullLimit}&raw_json=1`;
 
 app.use(express.static('public'));
 
@@ -14,7 +14,11 @@ app.get("/", async (req, res) => {
         const randomJoke = response.data.data.children[Math.floor(Math.random() * pullLimit)].data;
         const joke = {
             title: randomJoke.title,
-            body: randomJoke.selftext
+            body: randomJoke.selftext,
+            author: randomJoke.author,
+            link: randomJoke.url,
+            upvotes: randomJoke.ups,
+            comments: randomJoke.num_comments
         }
 
         res.render("index.ejs", { joke: joke });
